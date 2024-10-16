6a0595ed8242ffda97eaee06b2c31caf69eead86
ICY <ICY@blah.com>
2024-10-16 19:24:26 +0100

    MVP

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/shorten", shorten)
+	http.ListenAndServe(":8080", nil)
+}
+
+func shorten(w http.ResponseWriter, r *http.Request) {
+	urls := r.URL.Query()["url"]
+	for _, u := range urls {
+		shortened := shortenURL(u)
+		fmt.Fprintf(w, "Shortened URL: %s\n", shortened)
+	}
+}
+
+func shortenURL(u string) string {
+	if !strings.HasPrefix(u, "http://") && !strings.HasPrefix(u, "https://") {
+		u = "http://" + u
+	}
+	parsedURL, err := url.Parse(u)
+	if err != nil {
+		return "Invalid URL"
+	}
+	return parsedURL.Host
+}
